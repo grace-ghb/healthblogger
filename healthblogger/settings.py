b699b2ab417fdc6ddb320668916077fa18c1f0d0
grace-ghb <>
2024-01-02 21:46:16 +0000

    Change 'STATICFILES_STORAGE

--- healthblogger/settings.py
+++ healthblogger/settings.py
@@ -143,11 +143,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = [
-    'cloudinary_storage.storage.StaticHashedCloudinaryStorage',
-    'whitenoise.storage.CompressedManifestStaticFilesStorage',
-    ]
-
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
+    # 'cloudinary_storage.storage.StaticHashedCloudinaryStorage',
+    # 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+    
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
